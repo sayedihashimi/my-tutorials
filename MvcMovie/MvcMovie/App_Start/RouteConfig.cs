4396743306af1e96cdac40faf237d4ae96ae69c8
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2021-06-08 15:55:09 -0400

    adding movie controller

--- MvcMovie/MvcMovie/App_Start/RouteConfig.cs
+++ MvcMovie/MvcMovie/App_Start/RouteConfig.cs
@@ -15,6 +15,11 @@ namespace MvcMovie {
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                 name: "Hello",
+                 url: "{controller}/{action}/{name}/{id}"
+             );
         }
     }
 }
---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers {
+    public class HelloWorldController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult Welcome(string name ="Demo", int numTimes = 5) {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
