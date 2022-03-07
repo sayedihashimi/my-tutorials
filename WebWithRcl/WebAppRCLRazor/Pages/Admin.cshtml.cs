9d5baa960899f309d439d0f3d05a7254dcf8253a
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-03-07 12:39:52 -0500

    adding files

---
+++ WebWithRcl/WebAppRCLRazor/Pages/Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRCLRazor.Pages
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
